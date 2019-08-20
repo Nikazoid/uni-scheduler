9e04dc6cf8a13392f8be3ac47b6d34f57b6c0fbc
Nikazoid <w.nikolai.georgiev96@gmail.com>
2019-08-21 00:12:47 +0300

    branch init

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
