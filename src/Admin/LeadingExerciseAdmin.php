c99c2b626ee349b3242e8777f0115d2d423ed9af
Nick Georgiev <w.nikolai.georgiev96@gmail.com>
2019-08-24 13:09:37 +0300

    Lecturer/LeadingExercises Validation (#58)

--- src/Admin/LeadingExerciseAdmin.php
+++ src/Admin/LeadingExerciseAdmin.php
@@ -9,12 +9,60 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
+use Sonata\Form\Validator\ErrorElement;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class LeadingExerciseAdmin extends AbstractAdmin
 {
+    public function validate(ErrorElement $errorElement, $object)
+    {
+        $em = $this->getConfigurationPool()->getContainer()->get('doctrine')->getManager();
+
+        $lecturerID=$object->getLecturer()->getId();
+        $disciplineID=$object->getDiscipline()->getId();
+        $exerciseType=$object->getExerciseType();
+
+        $sql = "SELECT *
+                FROM leading_exercise
+                WHERE 
+                    lecturer_id = $lecturerID 
+                AND discipline_id = $disciplineID 
+                AND exercise_type = '" . "$exerciseType" . "'";
+
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $result = $stmt->fetchAll();
+
+        if (count($result)>0){
+            $errorElement
+                ->with('lecturer')
+                    ->addViolation('Toзи Преподавател вече преподава по съответната дисциплина')
+                ->end();
+        }
+
+        if ($exerciseType=$object->getExerciseType() == 'лекция')
+        {
+            $sql1 = "SELECT *
+                FROM leading_exercise
+                WHERE discipline_id = $disciplineID AND exercise_type = '" . "лекция" . "'";
+
+            $stmt = $em->getConnection()->prepare($sql1);
+            $stmt->execute();
+
+            $result = $stmt->fetchAll();
+
+            if (count($result)>0){
+                $errorElement
+                    ->with('exerciseType')
+                    ->addViolation('Лекцията по тази дисциплина е заета ')
+                    ->end();
+            }
+        }
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
--- src/Admin/LecturerAdmin.php
+++ src/Admin/LecturerAdmin.php
@@ -8,48 +8,46 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
 use Sonata\AdminBundle\Show\ShowMapper;
-use Sonata\Form\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
 
 class LecturerAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->with('Lecturer')
-            ->add('firstName')
-            ->add('middleName')
-            ->add('lastName')
-            ->add('leadingExercises', CollectionType::class, [
-                'label' => 'Водещи занятия',
-                'btn_add' => 'добави',
-            ], [
-                'admin_code' => LeadingExerciseAdmin::class,
-                'edit' => 'inline',
-                'inline' => 'table',
-            ])
-            ->add('academicTitle', ModelType::class, [
-                'label' => 'Академична Титла',
-                'btn_add' => false
-            ])
-            ->add('scientificTitle', ModelType::class, [
-                'label' => 'Научни Титли',
-                'multiple' => true,
-                'btn_add' => false
-            ])
-            ->add('room', null, [
-                'label' => 'Стая на преподавателя'
-            ])
-            ->add('contract', ChoiceType::class, [
-                'required' => false,
-                'choices' => [
-                    'Primary' => Lecturer::LECTURER_CONTRACT_PRIMARY,
-                    'Guest' => Lecturer::LECTURER_CONTRACT_GUEST,
-                    'Civil' => Lecturer::LECTURER_CONTRACT_CIVIL
-                ],
-            ])
-            ->add('estHours')
+            ->tab('Преподавател')
+                ->with('Имена', ['class' => 'col-md-6'])
+                    ->add('firstName')
+                    ->add('middleName')
+                    ->add('lastName')
+                ->end()
+                ->with('Титли', ['class' => 'col-md-6'])
+                    ->add('academicTitle', ModelType::class, [
+                        'label' => 'Академична Титла',
+                        'btn_add' => false
+                    ])
+                    ->add('scientificTitle', ModelType::class, [
+                        'label' => 'Научни Титли',
+                        'multiple' => true,
+                        'btn_add' => false
+                    ])
+                ->end()
+                ->with('Допълнителна Информация', ['class' => 'col-md-12'])
+                    ->add('room', null, [
+                        'label' => 'Стая на преподавателя'
+                    ])
+                    ->add('contract', ChoiceType::class, [
+                        'label' => 'Тип на договора',
+                        'choices' => [
+                            'Основен Трудов' => Lecturer::LECTURER_CONTRACT_PRIMARY,
+                            'Гост' => Lecturer::LECTURER_CONTRACT_GUEST,
+                            'Граждански' => Lecturer::LECTURER_CONTRACT_CIVIL
+                        ],
+                    ])
+                    ->add('estHours', null, [
+                        'label' => 'Брой полагащи се часове'
+                    ])
+                ->end()
             ->end()
         ;
     }
@@ -64,7 +62,8 @@ class LecturerAdmin extends AbstractAdmin
             ->add('academicTitle')
             ->add('scientificTitle')
             ->add('leadingExercises')
-            ->add('lastName')
+            ->add('room')
+            ->add('user')
             ->add('_action', null, [
                 'actions' => [
                     'show' => [],
@@ -79,13 +78,15 @@ class LecturerAdmin extends AbstractAdmin
     {
         $showMapper
             ->with('Lecturer')
-                ->add('scientificTitle')
-                ->add('academicTitle')
                 ->add('firstName')
                 ->add('middleName')
-                ->add('leadingExercises')
                 ->add('lastName')
+                ->add('user')
+                ->add('scientificTitle')
+                ->add('academicTitle')
+                ->add('leadingExercises')
                 ->add('estHours')
+                ->add('room')
                 ->add('contract')
             ->end()
         ;
